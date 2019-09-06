6092e376171d616d9ca9f01a91844f23fdea6a14
Rosstislav <randomadman@gmail.com>
2019-09-06 08:49:51 +0400

    laba DataBase

--- AbstractPizzeria/AbstractPizzeria/Customer.cs
+++ AbstractPizzeria/AbstractPizzeria/Customer.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AbstractPizzeria
 {
@@ -10,6 +12,10 @@ namespace AbstractPizzeria
     {
         public int Id { get; set; }
 
+        [Required]
         public string CustomerFIO { get; set; }
+
+        [ForeignKey("CustomerId")]
+        public virtual List<Request> Requests { get; set; }
     }
 }
